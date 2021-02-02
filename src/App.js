1c7bd5f8d297945461e2cd6cdea7c9f4c2de88ef
Jasveer-Shah <68518753+Jasveer-Shah@users.noreply.github.com>
2021-02-02 08:16:44 -0800

    CHNG:passing title as props to the header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from "./Header";
 
 function App() {
-
+     
 
   return (
       <div className='App'>
-         <Header/>
+         <Header title= "Amazon Cart"/>
 
            
       </div>
